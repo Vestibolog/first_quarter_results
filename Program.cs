f342e0014983865063ad7743becd6a353bebce89
Vestibolog <tarn@ngs.ru>
2022-07-02 21:12:59 +0700

    added short word count

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿string[] inputArray = 	{"hello", "2", "world", ":-)"};
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"{array[i]},  ");
+    }
+    Console.WriteLine();
+}
+
+int numberShortWords = 0;
+for (int i = 0; i < inputArray.GetLength(0); i++)
+{
+    if ( inputArray[i] .Length <= 3){
+        numberShortWords++;
+    }
+}
+
+
+PrintArray(inputArray);
+Console.WriteLine($"Количество слов <=3 символов = {numberShortWords}");
